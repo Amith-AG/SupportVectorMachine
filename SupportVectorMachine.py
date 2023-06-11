87b80abf1a71b3bd6fbb0e26136e7cf141a4c684
Amith A G <118121039+Amith-AG@users.noreply.github.com>
2023-06-11 15:50:08 +0530

    add SupportVectorMachine.py

---
+++ SupportVectorMachine.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+diabetes = pd.read_csv('diabetes.csv')
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'Outcome'], 
+diabetes['Outcome'], stratify=diabetes['Outcome'], random_state=66)
+from sklearn.svm import SVC
+svc = SVC()
+svc.fit(X_train, y_train)
+print("Accuracy on training set: {:.2f}".format(svc.score(X_train, y_train)))
+print("Accuracy on test set: {:.2f}".format(svc.score(X_test,y_test)))
